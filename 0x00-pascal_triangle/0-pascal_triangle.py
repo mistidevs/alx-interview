776cb168cc1ddf9cccdbd011229b21388d404597
mistidevs <mistidevs@gmail.com>
2024-04-23 12:14:27 +0300

    code (0-pascal_triangle.py) : Pycodestyle Fixes

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,32 +3,32 @@
 Using iteration to print the Pascals triangle
 """
 
+
 def pascal_triangle(n):
-  if n <= 0:
-    return []
-  
-  pascals = [[] for _ in range(n)]
+    if n <= 0:
+        return []
 
-  if n >= 1:
-    pascals[0].append(1)
-  if n >= 2:
-    pascals[1].append(1)
-    pascals[1].append(1)
+    pascals = [[] for _ in range(n)]
 
-  row = 1
-  
-  if n >= 3:
-    for i in range(n - 2):
-      pascals[row + 1].append(1)
-      
-      for j in range(len(pascals[row]) - 1):
-        pascals[row + 1].append(pascals[row][j] + pascals[row][j + 1])
+    if n >= 1:
+        pascals[0].append(1)
+    if n >= 2:
+        pascals[1].append(1)
+        pascals[1].append(1)
 
-      pascals[row + 1].append(1)
-      row += 1
+    row = 1
 
-  return pascals
+    if n >= 3:
+        for i in range(n - 2):
+            pascals[row + 1].append(1)
+            for j in range(len(pascals[row]) - 1):
+                pascals[row + 1].append(pascals[row][j] + pascals[row][j + 1])
+
+            pascals[row + 1].append(1)
+            row += 1
+
+    return pascals
 
 
 if __name__ == "__main__":
-  pascal_triangle()
+    pascal_triangle()
