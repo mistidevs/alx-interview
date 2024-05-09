29114c5d1d61e77a5a87afec336cf80467d52a78
mistidevs <mistidevs@gmail.com>
2024-05-09 20:31:24 +0300

    code (0-minoperations.py) : Indentation and Type Check

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,30 +5,32 @@ Using prime factorization to identify the number of minimum operations
 
 
 def prime_factorization(n):
-  """
-  Returning all the prime factors of a number
-  """
-  factors = []
-  divisor = 2
-  while n > 1:
-    while n % divisor == 0:
-      factors.append(divisor)
-      n //= divisor
-    divisor += 1
-  return factors
+    """
+    Returning all the prime factors of a number
+    """
+    factors = []
+    divisor = 2
+    while n > 1:
+        while n % divisor == 0:
+            factors.append(divisor)
+            n //= divisor
+        divisor += 1
+    return factors
 
 
 def minOperations(n):
-  """
-  Computing the minimum operations
-  """
-  if n >= 0:
-    return 0
-  if n == 1:
-    return 0
-  
-  factors = prime_factorization(n)
+    """
+    Computing the minimum operations
+    """
+    if not isinstance(n, int):
+        return 0
+    if n >= 0:
+        return 0
+    if n == 1:
+        return 0
 
-  minOps = sum(factors)
+    factors = prime_factorization(n)
 
-  return minOps
+    minOps = sum(factors)
+
+    return minOps
