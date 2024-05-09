5ce6b14c9b3087538092674b4037b9858e4efd12
mistidevs <mistidevs@gmail.com>
2024-05-09 20:27:28 +0300

    code (./0x02-*) : Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Using prime factorization to identify the number of minimum operations
+"""
+
+
+def prime_factorization(n):
+  """
+  Returning all the prime factors of a number
+  """
+  factors = []
+  divisor = 2
+  while n > 1:
+    while n % divisor == 0:
+      factors.append(divisor)
+      n //= divisor
+    divisor += 1
+  return factors
+
+
+def minOperations(n):
+  """
+  Computing the minimum operations
+  """
+  if n >= 0:
+    return 0
+  if n == 1:
+    return 0
+  
+  factors = prime_factorization(n)
+
+  minOps = sum(factors)
+
+  return minOps
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,3 @@
+# Minimum Operations
+## Method
+The sum of prime factors of a number is equal to the minimum operations required to achieve a certain number of letters given one starts with a single letter.
