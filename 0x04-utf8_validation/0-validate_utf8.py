cbeb9b40bd341437992cfca3d75bd4a876afd8fe
mistidevs <mistidevs@gmail.com>
2024-05-23 20:49:49 +0300

    code (0x04-utf8_validation) : Using Bit operations to check UTF8 encodings

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+UTF-8 validation module.
+"""
+
+
+def validUTF8(data):
+    """
+    Checks if a list of integers are valid UTF-8 encodings.
+    """
+    skip = 0
+    n = len(data)
+    for i in range(n):
+        if skip > 0:
+            skip -= 1
+            continue
+        if type(data[i]) != int or data[i] < 0 or data[i] > 0x10ffff:
+            return False
+        elif data[i] <= 0x7f:
+            skip = 0
+            span = 0
+        elif data[i] & 0b11111000 == 0b11110000:
+            span = 4
+        elif data[i] & 0b11110000 == 0b11100000:
+            span = 3
+        elif data[i] & 0b11100000 == 0b11000000:
+            span = 2
+        else:
+            return False
+        
+        if n - i >= span:
+            next_body = list(map(
+                lambda x: x & 0b11000000 == 0b10000000,
+                data[i + 1: i + span],
+            ))
+            if not all(next_body):
+                return False
+            skip = span - 1
+        else:
+            return False
+    return True
