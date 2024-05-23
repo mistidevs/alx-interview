8fc679413e0643b4a471bf7c5fef061255700d74
mistidevs <mistidevs@gmail.com>
2024-05-23 21:47:51 +0300

    code (0-validate_utf8.py) : Pycodestyle

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -14,7 +14,7 @@ def validUTF8(data):
         if skip > 0:
             skip -= 1
             continue
-        if type(data[i]) != int or data[i] < 0 or data[i] > 0x10ffff:
+        if not isinstance(data[i], int) or data[i] < 0 or data[i] > 0x10ffff:
             return False
         elif data[i] <= 0x7f:
             skip = 0
@@ -27,7 +27,7 @@ def validUTF8(data):
             span = 2
         else:
             return False
-        
+
         if n - i >= span:
             next_body = list(map(
                 lambda x: x & 0b11000000 == 0b10000000,
