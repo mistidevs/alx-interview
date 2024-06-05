9399fbb7f54b1629275b3550a6383bc43ccf13c6
mistidevs <mistidevs@gmail.com>
2024-06-05 10:18:31 -0700

    code (0-starwars_characters.js) : Fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,32 +1,30 @@
 #!/usr/bin/node
-const request = require('request')
+const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-
 function requestPromise (url) {
-    return new Promise((resolve, reject) => {
-	request(url, (response, error, body) => {
-	    if (error) {
-		reject(error);
-	    } else {
-		resolve(JSON.parse(body));
-	    }
-	});
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body));
+      }
     });
+  });
 }
 
-
 requestPromise(url)
-    .then(data => {
-	const characters = data.characters;
-	const charactersPromises = characters.map(character => requestPromise(character));
-	return Promises.all(characterPromises);
-    })
-    .then(characterData => {
-	characterData.forEach(data => {
-	    console.log(data.name);
-	});
-    })
-    .catch(error => {
-	console.error(error);
+  .then(data => {
+    const characters = data.characters;
+    const characterPromises = characters.map(character => requestPromise(character));
+    return Promise.all(characterPromises);
+  })
+  .then(characterData => {
+    characterData.forEach(data => {
+      console.log(data.name);
     });
+  })
+  .catch(error => {
+    console.error(error);
+  });
