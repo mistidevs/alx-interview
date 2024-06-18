df51a011b198f3e1516179443ae9619a242ad1ae
mistidevs <mistidevs@gmail.com>
2024-06-18 11:18:37 +0300

    code (0x08-making_change) : Making change with an algorithm

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Determining the fewest number of coins
+needed to meet a given amount total
+"""
+
+
+def makeChange(coins, total):
+    """
+    Using sorting and iteration to solve
+    the problem
+    """
+    if total <= 0 or not isinstance(total, int):
+        return 0
+
+    num_coins = 0
+    i = 0
+    coins.sort(reverse=True)
+
+    while i < len(coins):
+        if total >= coins[i]:
+            total = total - coins[i]
+            num_coins += 1
+        elif total == 0:
+            return num_coins
+        elif i == (len(coins) - 1):
+            return -1
+        elif total < coins[i]:
+            i += 1
+            continue
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,2 @@
+# Making Change
+Determining the fewest number of coins needed to meet a given amount total.
