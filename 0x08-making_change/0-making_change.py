867e88ebce443a77f045ebcc6f197336bf378d34
mistidevs <mistidevs@gmail.com>
2024-06-18 11:36:55 +0300

    code (0-making_change.py) : Using dynamic programming rather than the greedy approach to solve the problem

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,27 +7,17 @@ needed to meet a given amount total
 
 def makeChange(coins, total):
     """
-    Using sorting and iteration to solve
-    the problem
+    Dynamic programming approach to
+    solving the problem
     """
     if total <= 0 or not isinstance(total, int):
         return 0
 
-    if len(coins) == 0:
-        return 0
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
 
-    num_coins = 0
-    i = 0
-    coins.sort(reverse=True)
+    for coin in coins:
+        for x in range(coin, total + 1):
+            dp[x] = min(dp[x], dp[x - coin] + 1)
 
-    while i < len(coins):
-        if total >= coins[i]:
-            total = total - coins[i]
-            num_coins += 1
-        elif total == 0:
-            return num_coins
-        elif i == (len(coins) - 1):
-            return -1
-        elif total < coins[i]:
-            i += 1
-            continue
+    return dp[total] if dp[total] != float('inf') else -1
