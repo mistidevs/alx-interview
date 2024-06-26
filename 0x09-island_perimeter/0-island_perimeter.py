28ada3b0bca673ef3290686c7dd70364694d8141
mistidevs <mistidevs@gmail.com>
2024-06-26 11:38:50 +0300

    code (0-island_perimeter.py) : Improving algorithm to handle edge island areas

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,21 +4,25 @@ Calculating the perimeter of a island in a 2D grid
 """
 
 
-def perimeter_check(grid, x, y):
+def perimeter_check(grid, x, y, isle_x, isle_y):
     """
     Computing the perimeter of unit of land mass
     """
     perimeter = 0
-    if grid[x][y - 1] == 0:
+    # Left
+    if grid[x][y - 1] == 0 or y == 0:
         perimeter += 1
 
-    if grid[x][y + 1] == 0:
+    # Right
+    if grid[x][y + 1] == 0 or y == isle_y - 1:
         perimeter += 1
 
-    if grid[x + 1][y] == 0:
+    # Bottom
+    if grid[x + 1][y] == 0 or x == isle_x - 1:
         perimeter += 1
 
-    if grid[x - 1][y] == 0:
+    # Top
+    if grid[x - 1][y] == 0 or x == 0:
         perimeter += 1
 
     return perimeter
@@ -28,13 +32,16 @@ def island_perimeter(grid):
     """
     The main function to calculate perimeter
     """
-    perimeter = 0
-    isle_x = len(grid[0]) - 1
-    isle_y = len(grid) - 1
+    if not isinstance(grid, list):
+        return 0
 
-    for x in range(1, isle_x):
-        for y in range(1, isle_y):
+    perimeter = 0
+    isle_x = len(grid)
+    isle_y = len(grid[0])
+
+    for x in range(0, isle_x):
+        for y in range(0, isle_y):
             if grid[x][y] == 1:
-                perimeter += perimeter_check(grid, x, y)
+                perimeter += perimeter_check(grid, x, y, isle_x, isle_y)
 
     return perimeter
