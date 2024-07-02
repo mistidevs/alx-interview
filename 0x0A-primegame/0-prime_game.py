a0300e5e77ff2a0c1aad040994aba159c29e6818
mistidevs <mistidevs@gmail.com>
2024-07-02 22:22:55 +0300

    code (0-prime_game.py) : Removing dubug print statements

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -33,40 +33,30 @@ def isWinner(x, nums):
     maria_games_won = 0
     ben_games_won = 0
     for i in range(x):
-        print(f'Round {i + 1}')
         game_nums = [i for i in range(1, nums[i] + 1)]
         maria_score = 0
         ben_score = 0
         maria_turn = True
         while game_nums:
             prime_found = False
-            print(f'Started Round')
             for num in game_nums:
                 if is_prime(num):
-                    print(f'{num} is prime')
                     game_nums = [x for x in game_nums if x % num != 0]
                     prime_found = True
                     if maria_turn:
-                        print(f'Maria gets a point')
                         maria_score += 1
                         maria_turn = False
                     else:
-                        print(f'Ben gets a point')
                         ben_score += 1
                         maria_turn = True
                     break
             if not prime_found:
-                print('No primes left, ending')
                 break
         if maria_score > ben_score:
-            print('Maria wins a game')
             maria_games_won += 1
         else:
-            print('Ben wins a game')
             ben_games_won += 1
     if maria_games_won > ben_games_won:
-        print('Maria wins')
         return 'Maria'
     else:
-        print('Ben wins')
         return 'Ben'
